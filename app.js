eb343f6467984f631e287753b495fe6e70bb1cd7
davidemilizia <davidemilizia@gmail.com>
2026-09-04 12:36:09 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1783,6 +1783,28 @@ if (savedProducts) {
       JSON.parse(savedProducts);
   } catch(e){}
 }
+   const savedSize =
+  localStorage.getItem(
+    "size_data_" + cinemaName
+  );
+
+if (savedSize) {
+  try {
+    size = JSON.parse(savedSize);
+  } catch(e){}
+}
+
+const savedPostMix =
+  localStorage.getItem(
+    "postmix_data_" + cinemaName
+  );
+
+if (savedPostMix) {
+  try {
+    postMixProducts =
+      JSON.parse(savedPostMix);
+  } catch(e){}
+}
 }
    
 function loadCountsFromStorage() {
