c5977b9be80d265874fa5a69f70af567245ee8a0
davidemilizia <davidemilizia@gmail.com>
2026-09-07 20:15:05 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2211,7 +2211,6 @@ function readMatrix(file) {
         if (!wb.SheetNames || !wb.SheetNames.length) throw new Error("Nessun foglio trovato.");
         let combinedMatrix = [];
         wb.SheetNames.forEach(sheetName => {
-           wb.SheetNames.forEach(sheetName => {
           const sheetData = XLSX.utils.sheet_to_json(wb.Sheets[sheetName], { header: 1, defval: "", raw: true });
           if (sheetData && sheetData.length > 0) combinedMatrix = combinedMatrix.concat(sheetData);
         });
